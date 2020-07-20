0940194816ff2f32073b509f0813e837c8dce6f0
Kyle Perra <perra.kyle@gmail.com>
2020-07-20 21:42:32 +0000

    Added ability to handle unequal length words.

--- index.js
+++ index.js
@@ -4,17 +4,44 @@ function transposeTwoWords(inputArray) {
 
   var firstWord = inputArray[0];
   var secondWord = inputArray[1];
-
-  for (let i = 0; i < firstWord.length; i++) {
-    // if we are on the last iteration, do not add a newline
-    if (i === firstWord.length - 1) {
-      console.log(firstWord[i] + ' ' + secondWord[i]);
-    } else { // otherwise log respective characters and add newline to the end
-      console.log(firstWord[i] + ' ' + secondWord[i] + '\n');
-    }
+  var longestWord = inputArray[0];
+  if (secondWord.length > longestWord.length) {
+    longestWord = secondWord;
   }
 
+  for (let i = 0; i < longestWord.length; i++) {
+    if (firstWord[i] === undefined) {
+      // if we are on the last iteration, do not add a newline
+      if (i === longestWord.length - 1) {
+        console.log('  ' + secondWord[i]);
+      } else { // otherwise log respective characters and add newline to the end
+        console.log('  ' + secondWord[i] + '\n');
+      }
+    } else if (secondWord[i] === undefined) {
+      // if we are on the last iteration, do not add a newline
+      if (i === longestWord.length - 1) {
+        console.log(firstWord[i] + '  ');
+      } else { // otherwise log respective characters and add newline to the end
+        console.log(firstWord[i] + '  \n');
+      }
+    } else {
+      // if we are on the last iteration, do not add a newline
+      if (i === longestWord.length - 1) {
+        console.log(firstWord[i] + ' ' + secondWord[i]);
+      } else { // otherwise log respective characters and add newline to the end
+        console.log(firstWord[i] + ' ' + secondWord[i] + '\n');
+      }
+    }
+
+  }
 }
 
-var wordsToBeTransposed = ['hello', 'world'];
-transposeTwoWords(wordsToBeTransposed);
+var equalLengthWords = ['hello', 'world'];
+var longerFirstWord = ['helloo', 'world'];
+var longerSecondWord = ['hello', 'worlds'];
+console.log('Equal Length Words:\n')
+transposeTwoWords(equalLengthWords);
+console.log('\nLonger First Word:\n');
+transposeTwoWords(longerFirstWord);
+console.log('\nLonger Second Word:\n');
+transposeTwoWords(longerSecondWord);

9bd5c71c53dd24206061c181677a7362789d66e5
kyleperra <perra.kyle@gmail.com>
2020-11-26 15:39:10 -0800

    input text value will become the inner text value of the created list item

--- event-bubbling/app.js
+++ event-bubbling/app.js
@@ -3,22 +3,31 @@ const todoList = document.querySelector('#todo-list');
 // select the bold tag within the H2 for showcasing the number of items dynamically
 const todoNum = document.querySelector('.todo-num b');
 const mainTitle = document.querySelector('.main-title');
+const nameInput = document.querySelector('.name-input');
 // // all of the children of todoNum and only the children elements
 const items = todoList.children;
 
 // attach event listener
-button.addEventListener('click', function() {
+button.addEventListener('click', function(event) {
+  // because, when submitted, form items will refresh the page, using this function we cant prevent this default functionality
+  event.preventDefault();
   // code of what we want to run when the button is clicked
   console.log('thanks for clicking :)');
+  // creating the element
   const newItem = document.createElement('li');
+  // adding the class
   newItem.classList.add('item');
-  newItem.innerText = `item ${items.length + 1}`;
-
+  // adding the text
+  newItem.innerText = nameInput.value;
+  // appending to the todoList
   todoList.appendChild(newItem);
+
   todoNum.innerText = items.length;
 
   // create the event listener
-  newItem.addEventListener('click', deleteItem)
+  newItem.addEventListener('click', deleteItem);
+
+  console.log(nameInput);
 });
 
 // this will iterate through all of the items array and add an addEventListener

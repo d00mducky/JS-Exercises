ba1712b258a47c0266066cf965f8017e4379f24d
kyleperra <perra.kyle@gmail.com>
2020-11-26 14:31:26 -0800

    alters header style on button click

--- simple-events/app.js
+++ simple-events/app.js
@@ -2,19 +2,28 @@ const button = document.querySelector('#submit');
 const todoList = document.querySelector('#todo-list');
 // select the bold tag within the H2 for showcasing the number of items dynamically
 const todoNum = document.querySelector('.todo-num b');
-// all of the children of todoNum and only the children elements
-const items = todoList.children;
+const mainTitle = document.querySelector('.main-title');
 
 
-// attach event listener
+
+
+
+// // all of the children of todoNum and only the children elements
+// const items = todoList.children;
+
+// // attach event listener
+// button.addEventListener('click', function() {
+//   // code of what we want to run when the button is clicked
+//   console.log('thanks for clicking :)');
+//   const newItem = document.createElement('li');
+//   newItem.classList.add('item');
+//   newItem.innerText = `item ${items.length + 1}`;
+
+//   todoList.appendChild(newItem);
+//   todoNum.innerText = items.length;
+// });
+
 button.addEventListener('click', function() {
-  // code of what we want to run when the button is clicked
-  console.log('thanks for clicking :)');
-  const newItem = document.createElement('li');
-  newItem.classList.add('item');
-  newItem.innerText = `item ${items.length + 1}`;
-
-  todoList.appendChild(newItem);
-  todoNum.innerText = items.length;
+  mainTitle.style.color = 'red';
+  mainTitle.stlye.fontSize = '50px';
 });
-
